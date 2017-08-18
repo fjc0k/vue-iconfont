264b272075acf0961cefe581e2bd492b71550056
fjc0k <hello.funch@gmail.com>
2017-08-19 04:22:07 +0800

    🎨 Tweaks

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import icon from './icon';
 
-export const install = function(Vue, { label = 'icon' }) {
+icon.install = function(Vue, { label = 'icon' }) {
   if (install.installed) return;
   Vue.components(label, icon);
 };
 
 // auto install
 if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue);
+  icon.install(window.Vue);
 };
+
+export default icon;
