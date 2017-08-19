9c1cf8bd5cdf42728286d39ce16bbca3285b9bf2
fjc0k <hello.funch@gmail.com>
2017-08-19 16:59:50 +0800

    fix bugs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import icon from './icon';
 
 icon.install = function(Vue, { label = 'icon' } = {}) {
   if (icon.install.installed) return;
-  Vue.components(label, icon);
+  Vue.component(label, icon);
 };
 
 // auto install
