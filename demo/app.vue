73617e95af96fed10a652d277b48325d6ba2da88
fjc0k <fjc0kb@gmail.com>
2018-05-11 03:57:27 +0800

    demo: update iconfont

--- demo/app.vue
+++ demo/app.vue
@@ -2,6 +2,7 @@
   <div>
     <v-icon name="right" />
     <v-svg-icon name="right" />
+    <v-svg-icon name="zhongguo" style="font-size: 100px;" />
     <v-icon name="right" style="color: red; font-size: 2em;" />
     <v-svg-icon name="right" style="color: blue; font-size: 14px;" />
     <fab-icon name="github" />
