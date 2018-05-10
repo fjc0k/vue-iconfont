2a5bc3ff17354572b9c548fc88625854d8a69852
fjc0k <fjc0kb@gmail.com>
2018-05-11 03:30:51 +0800

    refactor: remove unnecessary codes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import getIcon from './getIcon'
-import { ICON_COMPONENTS_REGISTER } from './constant'
 
 const Icon = getIcon()
 
@@ -11,18 +10,7 @@ Icon.install = (Vue, options) => {
   if (!Array.isArray(options)) options = [options]
 
   options.forEach($options => {
-    if (!Vue[ICON_COMPONENTS_REGISTER]) {
-      Vue[ICON_COMPONENTS_REGISTER] = Object.create(null)
-    }
-
-    const key = `${$options.tag}/${$options.type}/${$options.prefix}`
-
-    if (key in Vue[ICON_COMPONENTS_REGISTER]) return
-
-    Vue[ICON_COMPONENTS_REGISTER][key] = true
-
     const Icon = getIcon($options)
-
     Vue.component(
       $options.tag || Icon.name,
       Icon
